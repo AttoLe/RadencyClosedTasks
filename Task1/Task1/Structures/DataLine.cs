6dd1bda4cc47e61b2812ae2af65e7437230f6eb4
AttoLe <92326233+AttoLe@users.noreply.github.com>
2023-02-21 02:21:47 +0200

    Create DataLine.cs
    .

---
+++ Task1/Task1/Structures/DataLine.cs
@@ -0,0 +1,11 @@
+﻿namespace Task1.Structures;
+
+public struct DataLine
+{
+    public string Name = string.Empty, City = string.Empty, Service = string.Empty;
+    public decimal Payment = 0;
+    public DateOnly Date = default;
+    public long AccountNumber = 0;
+    
+    public DataLine() { }
+}
