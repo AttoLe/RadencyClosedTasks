b1faf8da6fd52562134acdc9cf3e6d9e1b6ac4df
AttoLe <sergii.kucherenko.jr@gmail.com>
2023-02-28 20:24:20 +0200

    task3
    fixed few DTOs, added task3 without input handler

--- Task2/Task2/Mappers/DTOs/BookWReviewNumDTO.cs
+++ Task2/Task2/Mappers/DTOs/BookWReviewNumDTO.cs
@@ -5,6 +5,8 @@ public class BookWReviewNumDTO
     public int Id { get; set; }
     public string Title { get; set; }
     public string Author { get; set; }
+    public string Genre { get; set; }
+    public string Cover { get; set; }
     public decimal AvgRating { get; set; }
     public int ReviewsNumber { get; set; }
 }
